384cbdbb14c00b3249474e7f485d390217f6bf89
Lucas Ramos <108039029+lucasramos95@users.noreply.github.com>
2022-07-15 10:15:27 -0300

    6th Commit - Modificações no HTML e código Js

---
+++ js/main.js
@@ -0,0 +1,75 @@
+        var audioPlayer = document.getElementById('audioplayer');
+        var loaded = false;
+        
+        var playBtn = document.getElementById ('playBtn');
+        var pauseBtn = document.getElementById ('pauseBtn');
+
+        pauseBtn.addEventListener('click', (e)=> {
+            e.preventDefault();
+
+            playBtn.style.display = "inline";
+            pauseBtn.style.display = "none";
+            audioPlayer.pause();
+            return false;
+        });
+
+        playBtn.addEventListener('click', (e)=> {
+            e.preventDefault();
+
+            playBtn.style.display = "none";
+            pauseBtn.style.display = "inline";
+            audioPlayer.play();
+            return false;
+        });
+
+        const playSong = (file) => {
+
+            if(loaded==false){
+                audioPlayer.innerHTML = `<source src="`+file+`" type="audio/wav" />`;
+                loaded = true;
+            }
+
+            audioPlayer.play();
+
+            playBtn.style.display = "none";
+            pauseBtn.style.display = "inline";
+        
+        }
+
+        document.querySelectorAll('.main_col').forEach (item => {
+
+            item.addEventListener('click',event=> {
+                let image = item.getAttribute('data-image');
+                let artist = item.getAttribute('data-artist');
+                let song = item.getAttribute('data-song');
+                let file = item.getAttribute('data-file');
+
+                let playerArtistComponent = document.getElementsByClassName('player_artist');
+
+                playerArtistComponent[0].innerHTML = `
+                <img src="`+image+`"/>
+                <h3>`+artist+`<br/><span>`+song+`</span></h3>
+                `;
+
+                playSong(file);
+
+            })
+
+            item.addEventListener('click',event=> {
+                let image = item.getAttribute('data-image');
+                let artist = item.getAttribute('data-artist');
+                let song = item.getAttribute('data-song');
+                let file = item.getAttribute('data-file');
+
+                let playerArtistComponent = document.getElementsByClassName('player_artist');
+
+                playerArtistComponent[1].innerHTML = `
+                <img src="`+image+`"/>
+                <h3>`+artist+`<br/><span>`+song+`</span></h3>
+                `;
+
+                playSong(file);
+
+            })
+
+        });
